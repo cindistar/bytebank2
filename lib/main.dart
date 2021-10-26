2530b249347695950f8a6764ced01dd7a3c22b00
Cindi Nishimoto <cindi_nishi@hotmail.com>
2021-10-26 14:51:48 -0300

    bytebank 2

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bytebank/components/theme.dart';
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class LogObserver extends BlocObserver {
+ @override
+  void onChange(BlocBase bloc, Change change) {
+    print('${bloc.runtimeType} > $change');
+    super.onChange(bloc, change);
+  }
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Bloc.observer = LogObserver();
+    return MaterialApp(
+      theme: bytebankTheme,
+      home: DashboardContainer(),
+    );
+  }
+}
